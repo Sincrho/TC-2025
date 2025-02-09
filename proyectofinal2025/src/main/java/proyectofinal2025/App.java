c95eb7da4ff515c055c5f4425807d09a342b3a4d
Santiago Lopez <slopez971216@gmail.com>
2025-02-09 17:56:24 -0300

    first commit

---
+++ proyectofinal2025/src/main/java/proyectofinal2025/App.java
@@ -0,0 +1,13 @@
+package proyectofinal2025;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
